d36cbfe3dce293a6e923e650d4c0d2d2fa7eeab6
OracleSkies <pjayjaydonasco@gmail.com>
2023-12-24 16:55:08 +0800

    security Login now functional
    added feature that checks the values of username and password from the database and will log the security user in if both pass and username exist

--- digitalParkingLotSescurityLog.py
+++ digitalParkingLotSescurityLog.py
@@ -1,6 +1,5 @@
 from tkinter import *
-from tkinter import ttk
-from tkinter import messagebox
+from tkinter import ttk, messagebox
 from PIL import ImageTk,Image
 import mysql.connector
 import csv
@@ -16,15 +15,48 @@ scanWindowBG=Label(scanWindow,image=scanWindowBackEnd)
 scanWindowBG.grid(row=0, column=0)
 scanWindow.resizable(False,False)
 
-def checkUserToDatabase():
-    return print("check")
+Database = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    passwd = 'password123',
+    database = 'registeredParkingUsersDatabase',
+)
+cursorMain = Database.cursor()
+cursorMain.execute("CREATE DATABASE IF NOT EXISTS registeredParkingUsersDatabase")
 
-def dbquery():
-    return print("query")
+def checkUserToDatabase():
+    sqlCommand = "SELECT * FROM registeredUsers WHERE studentNumber = %s" 
+    scannedStdnNo = scanIDfield.get()
+    stdnNumber = (scannedStdnNo, )
+    cursorMain.execute(sqlCommand, stdnNumber)
+    result = cursorMain.fetchone()
+
+    #checker lang to. Trash this
+    if result:
+        print("data exist")
+    else: #data does not exist
+        #open registration Window
+        #put message box and additional ifelse logic here
+        OpenParkingRegistrationWindow()
+        print("data doesnt exist")
+
+def dbquery(): #temporary function
+    dataQuery = Tk()
+    dataQuery.title("registered Query")
+    dataQuery.geometry('800x600')
+    cursorMain.execute("SELECT * FROM registeredUsers")
+    result = cursorMain.fetchall()
+    for index, tableRow in enumerate(result):
+        num = 0
+        for tableColumn in tableRow:
+            dataQueryLabel = Label(dataQuery,text=tableColumn) 
+            dataQueryLabel.grid(row=index, column=num, padx=5)
+            num += 1
     
 def releaseGrab(_window):
     _window.grab_release()
     _window.destroy()
+    scanWindow.destroy()
 
 def OpenSecurityLogInWindow():
     securityLogInWindow = Toplevel(scanWindow)
@@ -37,6 +69,13 @@ def OpenSecurityLogInWindow():
     secLogWinBG.image = securityLogInWindowBackground
     secLogWinBG.grid(row=0, column=0)
     securityLogInWindow.resizable(False,False)
+
+    DB = mysql.connector.connect(
+        host = 'localhost',
+        user = 'root',
+        passwd = 'password123',
+        database = 'sercurityAccountDatabase',
+    )
 
     loginText= Label(securityLogInWindow, text="SECURITY LOG IN",font="berlinsans",bg="darkgreen",width=20, height=1,)
     loginText.place(relx=0.27, rely=0.28,)
@@ -57,6 +96,20 @@ def OpenSecurityLogInWindow():
     def securityRegistration():
         OpenSecurityRegistration()
         securityLogInWindow.destroy()
+    
+    def accountLogIn():
+        cursor = DB.cursor()
+        sqlCommand = "SELECT * FROM registeredSecurity WHERE username = %s AND password = %s"
+        usernameInput = usernameField.get()
+        passwordInput = pwField.get()
+        account = (usernameInput, passwordInput)
+        cursor.execute(sqlCommand, account)
+        result = cursor.fetchone()
+        if result:
+            #messagebox
+            securityLogInWindow.destroy()
+        else:
+            print("data doesnt exist")
 
     #entryfields
     usernameField= Entry(securityLogInWindow, fg="black",border=2, bg="white", font=("Microsoft YaHei UI Light", 10),width=17)
@@ -75,13 +128,12 @@ def OpenSecurityLogInWindow():
     registerButton= Button(securityLogInWindow, text="REGISTER", bg="darkgreen" ,width=20, command=securityRegistration)
     registerButton.place(relx= 0.32, rely=0.78)
 
-    loginButton= Button(securityLogInWindow, text="LOG IN", bg="darkgreen" ,width=20)
+    loginButton= Button(securityLogInWindow, text="LOG IN", bg="darkgreen" ,width=20, command=accountLogIn)
     loginButton.place(relx= 0.32, rely=0.68)
 
     securityLogInWindow.protocol("WM_DELETE_WINDOW",lambda: releaseGrab(securityLogInWindow))
-    securityLogInWindow.grab_set()
+    securityLogInWindow.grab_set()   
 
-    
 def OpenSecurityRegistration():
     secRegWin = Toplevel(scanWindow)
     secRegWin.title("PNC Parking Security Account Registration")
@@ -94,14 +146,62 @@ def OpenSecurityRegistration():
     secRegWinBG.grid(row=0, column=0)
     secRegWin.resizable(False,False)
 
+    DB = mysql.connector.connect(
+        host = 'localhost',
+        user = 'root',
+        passwd = 'password123',
+        database = 'sercurityAccountDatabase',
+    )
+    cursor = DB.cursor()
+    cursor.execute("CREATE DATABASE IF NOT EXISTS sercurityAccountDatabase")
+    cursor.execute("CREATE TABLE IF NOT EXISTS registeredSecurity(\
+        userID INT AUTO_INCREMENT PRIMARY KEY,\
+        firstName VARCHAR(255),\
+        lastName VARCHAR(255),\
+        username VARCHAR(255),\
+        password VARCHAR(255))")
+    '''
+    idagdag to kapag nacode na ung file handling ng pictures
+    userPhoto VARCHAR(255)
+    '''
+
     def registerSecurity():
-        return
+        sqlCommand = "INSERT INTO registeredSecurity (firstName, lastName, username, password) VALUES (%s, %s, %s, %s)"
+        values = (fNameField.get(), lNameField.get(), usernameField.get(), pwField.get())
+        cursor.execute(sqlCommand, values)
+        DB.commit()
+        messagebox.showinfo("Security Account Registration","Account Successfully Registered")
+
     
-    def dbquery():
-        return
+    def fieldContentLogic(_var, _field):
+        if _field == pwField:
+            if _var.get() !="                             Password":
+                _field.config(show="*")
+            else:
+                pwField.config(show="")
+        elif _field == confirmPwField:
+            if _var.get() != "                        Confirm Password":
+                _field.config(show="*")
+            else:
+                _field.config(show="")
+
+    def dbquery(): #temporary function
+        dataQuery = Tk()
+        dataQuery.title("Security account Query")
+        dataQuery.geometry('800x600')
+        cursor.execute("SELECT * FROM registeredSecurity")
+        result = cursor.fetchall()
+        for index, tableRow in enumerate(result):
+            num = 0
+            for tableColumn in tableRow:
+                dataQueryLabel = Label(dataQuery,text=tableColumn) 
+                # add index to x to get the specific field u want (eg x[0] would give you the 0th column of the database which is the first name) 
+                dataQueryLabel.grid(row=index, column=num, padx=5)
+                num += 1
     
-    def clearDB():
-        return
+    def clearDB(): #temporary function
+        cursor.execute('DELETE FROM registeredSecurity')
+        DB.commit()
 
     def varTrace(var, index, mode):
         fieldContentLogic(pwVar,pwField)
@@ -174,6 +274,140 @@ def OpenSecurityRegistration():
     secRegWin.protocol("WM_DELETE_WINDOW", lambda: releaseGrab(secRegWin))
     secRegWin.grab_set()
 
+def OpenParkingRegistrationWindow():
+    parkRegWin = Toplevel(scanWindow)
+    parkRegWin.title("PNC Parking Registration")
+    parkRegWin.geometry("800x450")
+    parkRegWin.iconbitmap("PNCLogo.ico")
+    parkRegWinCallImage=Image.open("registrationWindowBG.png")
+    parkRegWinBackground=ImageTk.PhotoImage(parkRegWinCallImage)
+    parkRegWinBG=Label(parkRegWin,image=parkRegWinBackground)
+    parkRegWin.image = parkRegWinBackground
+    parkRegWinBG.grid(row=0, column=0)
+    parkRegWin.resizable(False,False)
+
+    DB = mysql.connector.connect(
+        host = 'localhost',
+        user = 'root',
+        passwd = 'password123',
+        database = 'registeredParkingUsersDatabase',
+    )
+    cursor = DB.cursor()
+    cursor.execute("CREATE DATABASE IF NOT EXISTS registeredParkingUsersDatabase")
+    cursor.execute("CREATE TABLE IF NOT EXISTS registeredUsers (\
+        userID INT AUTO_INCREMENT PRIMARY KEY,\
+        firstName VARCHAR(255), \
+        lastName VARCHAR(255), \
+        studentNumber INT(20), \
+        department VARCHAR(255), \
+        vehicleType VARCHAR(255))")
+
+    '''
+        idagdag to kapag na-code na ung file handling for pictures
+        userPhoto VARCHAR(255), \ # gumamit ng VARCHAR kasi ififile handle ung name ng image file
+        vechiclePhoto VARCHAR(255)\
+        '''
+    
+    def registerUser():
+        cursor.execute("CREATE DATABASE IF NOT EXISTS registeredParkingUsersDatabase")
+        #table
+        cursor.execute("CREATE TABLE IF NOT EXISTS registeredUsers (\
+        userID INT AUTO_INCREMENT PRIMARY KEY,\
+        firstName VARCHAR(255), \
+        lastName VARCHAR(255), \
+        studentNumber INT(20), \
+        department VARCHAR(255), \
+        vehicleType VARCHAR(255))")
+
+        sqlCommand = "INSERT INTO registeredUsers (firstName, lastName, studentNumber, department, vehicleType) VALUES (%s, %s, %s, %s, %s)"
+        values = (fNameField.get(), lNameField.get(), StudentNoField.get(), deptOptionsDrpDwn.get(), vehicleOptionsDrpDwn.get())
+        cursor.execute(sqlCommand, values)
+        DB.commit()
+        messagebox.showinfo("Parking registration", "Registration Sucessful")
+
+    def goToScanWindow():
+        parkRegWin.destroy()
+
+    def dbquery(): #this function is temporary. This just shows all data in the table
+        dataQuery = Tk()
+        dataQuery.title("Parking registered users Query")
+        dataQuery.geometry('800x600')
+        cursor.execute("SELECT * FROM registeredUsers")
+        result = cursor.fetchall()
+        for index, tableRow in enumerate(result):
+            num = 0
+            for tableColumn in tableRow:
+                dataQueryLabel = Label(dataQuery,text=tableColumn) 
+                # add index to x to get the specific field u want (eg x[0] would give you the 0th column of the database which is the first name) 
+                dataQueryLabel.grid(row=index, column=num, padx=5)
+                num += 1
+
+    def clearDB(): #temporary function
+        cursor.execute('DELETE FROM registeredUsers')
+        DB.commit()
+
+    def fNameClearOnClick(event):
+        fNameField.delete(0,END)
+    def lNameClearOnClick(event):
+        lNameField.delete(0,END)
+    def studentNoClearOnClick(event):
+        StudentNoField.delete(0,END)
+
+    #text/labels
+    heading = Label(parkRegWin, text="Registration Form",bg="darkgreen", font=("Microsoft YaHei UI Light", 15,"bold"),width=40)
+    heading.place(relx=0.2, rely=0.2)
+
+    LineLang=  Label(parkRegWin, text="   ",bg="black", font=("Microsoft YaHei UI Light", 1,"bold"),width=240)
+    LineLang.place(relx=0.2,rely=0.625)
+
+    Vehicle= Label(parkRegWin, text="  VEHICLE  ",font=("Microsoft YaHei UI Light", 12,"bold"),width=14)
+    Vehicle.place(relx=0.2,rely=0.675)
+
+    #input/entry
+    fNameField= Entry(parkRegWin, width=20,fg="black",border=2, bg="white", font=("Microsoft YaHei UI Light", 10))
+    fNameField.place(relx=0.4, rely=0.3)
+    fNameField.insert(0,"First Name")
+    fNameField.bind("<Button-1>",fNameClearOnClick)
+
+    lNameField= Entry(parkRegWin, width=20, fg="black",border=2, bg="white", font=("Microsoft YaHei UI Light",10))
+    lNameField.place(relx=0.6, rely=0.3)
+    lNameField.insert(0,"Last Name")
+    lNameField.bind("<Button-1>",lNameClearOnClick)
+
+    StudentNoField= Entry(parkRegWin, width=43, fg="black", border=2, bg="white",font=("Microsoft YaHei UI Light",10))
+    StudentNoField.place(relx=0.4,rely=0.425)
+    StudentNoField.insert(0,"Faculty / Student No.")
+    StudentNoField.bind("<Button-1>",studentNoClearOnClick)
+
+    #Combo Boxes
+    deptOptionsDrpDwn = ttk.Combobox(parkRegWin, value = ["Select Department","College of Engineering", "College of Education", "College of Arts and Sciences", "College of Allied Health and Sciences", "College of Business Administration and Accountancy", "College of Computing Studies"])
+    deptOptionsDrpDwn.current(0)
+    deptOptionsDrpDwn.place(relx=0.4, rely=0.525, width=305)
+
+    vehicleOptionsDrpDwn = ttk.Combobox(parkRegWin, value = ["Select Type of Vehicle","Bike", "E-Bike", "Motorcycle"," Car"])
+    vehicleOptionsDrpDwn.current(0)
+    vehicleOptionsDrpDwn.place(relx=0.4,rely=0.672)
+
+    #Buttons
+    PicHolder= Button(parkRegWin, text="PHOTO HERE", bg="white", width=20,height=8)
+    PicHolder.place(relx=0.2, rely=0.3)
+
+    VehiclePDF= Button(parkRegWin, text="Upload Vehicle PDF", bg="white", width=20)
+    VehiclePDF.place(relx=0.6, rely=0.672)
+
+    Reg_button = Button(parkRegWin, text="Register", bg="darkgreen" ,font=("Microsoft YaHei UI Light",10,"bold"),width=18, command=registerUser)
+    Reg_button.place(relx= 0.4, rely=0.85)
+
+    backButton = Button(parkRegWin, text="Go Back", bg="gray" ,font=("Microsoft YaHei UI Light",10,"bold"),width=10, command=goToScanWindow)
+    backButton.place(relx= 0.7, rely=0.85)
+
+    #temporary buttons
+    test_button = Button(parkRegWin, text="Query", bg="yellow" ,font=("Microsoft YaHei UI Light",10,"bold"),width=18, command=dbquery)
+    test_button.place(relx= 0.4, rely=0.75)
+
+    cleardb_button = Button(parkRegWin, text="Clear database", bg="yellow" ,font=("Microsoft YaHei UI Light",10,"bold"),width=12, command=clearDB)
+    cleardb_button.place(relx= 0.65, rely=0.75)
+
 #label
 ParkingLotScan= Label(scanWindow, text="Parking Lot Scanner",font="berlinsans",bg="darkgreen",width=20, height=1,)
 ParkingLotScan.place(relx=0.27, rely=0.32,)
@@ -195,5 +429,4 @@ queryButton= Button(scanWindow, text="query", bg="darkgreen" ,font=("Microsoft Y
 queryButton.place(relx=0.4,rely=0.8)
 
 OpenSecurityLogInWindow()
-#scanWindow.protocol("WM_DELETE_WINDOW", lambda:releaseGrab(scanWindow))
 scanWindow.mainloop()
