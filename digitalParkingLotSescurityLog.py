f2c1232eed15efbbe5aee7d01b8970e3d4a4984e
OracleSkies <pjayjaydonasco@gmail.com>
2023-12-24 12:12:44 +0800

    Update digitalParkingLotSescurityLog.py

--- digitalParkingLotSescurityLog.py
+++ digitalParkingLotSescurityLog.py
@@ -4,81 +4,56 @@ from tkinter import messagebox
 from PIL import ImageTk,Image
 import mysql.connector
 import csv
+#scanWindow is root
+scanWindow = Tk()
+scanWindow.title("PNC Parking Scan")
+scanWindow.geometry("400x315")
+scanWindow.iconbitmap("PNCLogo.ico")
+scanWindowCallImage=Image.open("scanWindowBG.png")
+scanWindowBackEnd=ImageTk.PhotoImage(scanWindowCallImage)
+scanWindowBG=Label(scanWindow,image=scanWindowBackEnd)
+scanWindowBG.grid(row=0, column=0)
+scanWindow.resizable(False,False)
 
-secLogWindow = Tk()
-secLogWindow.title("PNC Parking")
-secLogWindow.geometry("800x450")
-secLogWindow.iconbitmap("PNCLogo.ico")
-image_0=Image.open("main_securitywindow_bg.png")
-bck_end=ImageTk.PhotoImage(image_0)
-lbl=Label(secLogWindow,image=bck_end)
-lbl.grid(row=0, column=0)
+def checkUserToDatabase():
+    return print("check")
 
-def openSecurityRegisterWindow():
-    messagebox.showinfo("Sign Up", "SIGN UP")
-    secLogWindow.destroy()
-    with open('securityLoginWindow.py', 'r') as file:
-        pythonCode = file.read()
-        exec(pythonCode)
+def dbquery():
+    return print("query")
 
-'''
-def openRegisterWindow():
-    regWindow = Tk()
-    regWindow.title("PNC Parking Registration")
-    regWindow.geometry("800x450")
-    regWindow.iconbitmap("PNCLogo.ico")
-    image_0=Image.open("main_regwindow_bg.png")
-    bck_end=ImageTk.PhotoImage(image_0)
-    lbl=Label(root,image=bck_end)
+def securityLogInWindow():
+    securityLogInWindow = Tk()
+    securityLogInWindow.title("PNC Security Log In")
+    securityLogInWindow.geometry("397x400")
+    securityLogInWindow.iconbitmap("PNCLogo.ico")
+    securityLogInWindowCallImage=Image.open("securityLogInWindowBG.png")
+    securityLogInWindowBackEnd=ImageTk.PhotoImage(securityLogInWindowCallImage)
+    lbl=Label(securityLogInWindow, image=securityLogInWindowBackEnd)
     lbl.grid(row=0, column=0)
-    root.resizable(False,False)
-'''
-def newWindowTest(): #this window is only temporary TRASH THIS LATER
-    ''''
-    newWindow = Tk()
-    newWindow.title("New Window")
-    newWindow.geometry("800x450")
-    newWindow.iconbitmap("PNCLogo.ico")
-    image_1=Image.open("main_regwindow_bg.png")
-    bck_end=ImageTk.PhotoImage(image_1)
-    registerButton = Button(newWindow, text="Register", command=openRegisterWindow)
-    registerButton.grid(row=0, column=1, padx=10,pady=10)
-    '''
-    with open('registrationWindow.py', 'r') as file:
-        pythonCode = file.read()
-        exec(pythonCode)
-    
+    securityLogInWindow.resizable(False,False)
 
-def closeLogInWindow():
-    #maglagay ng logic later for the login
+#label
+ParkingLotScan= Label(scanWindow, text="Parking Lot Scanner",font="berlinsans",bg="darkgreen",width=20, height=1,)
+ParkingLotScan.place(relx=0.27, rely=0.32,)
 
-    #
 
-    secLogWindow.destroy()
-    newWindowTest() #change this to parking log window
+#text
+ScanID= Label(scanWindow,text="Scan ID", font="berlinsans", width=12, height=1)
+ScanID.place(relx=0.258,rely=0.47)
 
-#text/labels
-loginText= Label(secLogWindow, text="SECURITY LOG IN",font="berlinsans",bg="darkgreen",width=20, height=1,)
-loginText.place(relx=0.39, rely=0.35,)
 
-#entryfields
-usernameField= Entry(secLogWindow, fg="gray",border=2, bg="white", font=("Microsoft YaHei UI Light", 8),width=17)
-usernameField.place(relx=0.44, rely=0.47)
-usernameField.insert(0,"Username")
+#entry/field
+scanIDfield= Entry(scanWindow, width=20,border=2, bg="white")
+scanIDfield.place(relx=0.33,rely=0.54)
 
-pwField= Entry(secLogWindow, fg="gray",border=2, bg="white", font=("Microsoft YaHei UI Light", 8),width=17)
-pwField.place(relx=0.44, rely=0.59)
-pwField.insert(0,"Password")
 
-#buttons
-'''
-forgotPwButton= Button(root, text="Forgot Password?",bg="white")
-forgotPwButton.place(relx=0.495, rely=0.68)
-'''
-loginButton= Button(secLogWindow, text="LOG IN", bg="darkgreen" ,width=20, command=closeLogInWindow)
-loginButton.place(relx= 0.41, rely=0.73)
+#Button
+confirmButton= Button(scanWindow, text="Confirm", bg="darkgreen" ,font=("Microsoft YaHei UI Light",8,"bold"),width=10, command=checkUserToDatabase)
+confirmButton.place(relx=0.4,rely=0.7)
 
-signUpButton= Button(secLogWindow, text="SIGN UP", bg="darkgreen" ,width=20, command=openSecurityRegisterWindow)
-signUpButton.place(relx= 0.41, rely=0.8)
+#temporary button. trash this later
+queryButton= Button(scanWindow, text="query", bg="darkgreen" ,font=("Microsoft YaHei UI Light",8,"bold"),width=10, command=dbquery)
+queryButton.place(relx=0.4,rely=0.8)
 
-secLogWindow.mainloop()
+securityLogInWindow()
+scanWindow.mainloop()
