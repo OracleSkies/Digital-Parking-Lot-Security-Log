2a2e85111107dd545d4ccb636e4d6b76f80fc850
zaig <philiplucero4@gmail.com>
2023-12-29 18:30:43 +0800

    scanWindowUPDATES

--- scanWindow.py
+++ scanWindow.py
@@ -12,9 +12,9 @@ need maglagay nung time aspect para sa time in and out
 #window
 root = Tk()
 root.title("PNC Parking")
-root.geometry("400x315")
+root.geometry("400x314")
 root.iconbitmap("PNCLogo.ico")
-image_0=Image.open("scanWindowBG.png")
+image_0=Image.open("scanWindowBGupdated.png")
 bck_end=ImageTk.PhotoImage(image_0)
 lbl=Label(root,image=bck_end)
 lbl.grid(row=0, column=0)
@@ -24,7 +24,7 @@ DB = mysql.connector.connect(
     host = 'localhost',
     user = 'root',
     passwd = 'password123',
-    database = 'registeredParkingUsersDatabase',
+    
 )
 #print(DB) #check if connected to MySQL
 
@@ -56,27 +56,31 @@ def checkUserToDatabase():
     else:
         print("data doesnt exist")
 
+#timeholder
+DTholder= Label(root,text="January 1, 2023 | 10:30 AM", font=("Segoe",10),bg="#f8faf7", width=25, height=1)
+DTholder.place(relx=0.53, rely=0.02)
+
 #label
-ParkingLotScan= Label(root, text="Parking Lot Scanner",font="berlinsans",bg="darkgreen",width=20, height=1,)
-ParkingLotScan.place(relx=0.27, rely=0.32,)
+ParkingLotScan= Label(root, text="Parking Lot Scanner",font="berlinsans",bg="darkgreen",width=21, height=1,)
+ParkingLotScan.place(relx=0.249, rely=0.35,)
 
 
 #text
 ScanID= Label(root,text="Scan ID", font="berlinsans", width=12, height=1)
-ScanID.place(relx=0.258,rely=0.47)
+ScanID.place(relx=0.258,rely=0.49)
 
 
 #entry/field
 scanIDfield= Entry(root, width=20,border=2, bg="white")
-scanIDfield.place(relx=0.33,rely=0.54)
+scanIDfield.place(relx=0.33,rely=0.56)
 
 
 #Button
 confirmButton= Button(root, text="Confirm", bg="darkgreen" ,font=("Microsoft YaHei UI Light",8,"bold"),width=10, command=checkUserToDatabase)
-confirmButton.place(relx=0.4,rely=0.7)
+confirmButton.place(relx=0.4,rely=0.72)
 
 #temporary button. trash this later
 queryButton= Button(root, text="query", bg="darkgreen" ,font=("Microsoft YaHei UI Light",8,"bold"),width=10, command=dbquery)
-queryButton.place(relx=0.4,rely=0.8)
+queryButton.place(relx=0.4,rely=0.85)
 
 root.mainloop()
